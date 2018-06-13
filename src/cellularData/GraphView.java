f65e472b1fa2a1d32c1845f715f65bee442cdeb6
PSMESTUDENTS <boewilliam@gmail.com>
2018-06-13 12:29:20 -0700

    Tried to set up the beginning of our program.

--- src/cellularData/GraphView.java
+++ src/cellularData/GraphView.java
@@ -2,12 +2,14 @@ package cellularData;
 
 import javafx.scene.chart.Axis;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 
 import javax.sound.sampled.Line;
 
 public class GraphView extends LineChart{ //This is just a skeleton and the start of the program so that we have an idea of what this program is supposed to do.
     NumberAxis xAxis;
     NumberAxis yAxis;
+    DataModel model;
     public GraphView(NumberAxis xAxis, NumberAxis yAxis) {
         super(xAxis, yAxis);
     }
--- src/cellularData/LineChart.java
+++
@@ -1,7 +0,0 @@
-package cellularData;
-
-public class LineChart {
-    public LineChart(GraphView xAxis , GraphView yAxis){ //ToDo This class may not be neccessary along with NumberAxis
-
-    }
-}
